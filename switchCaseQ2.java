f627459ff8006f2dc68dd7b9be1fc51320d26cfc
Kaif <mrkaifansari000786@gmail.com>
2025-11-26 18:28:08 +0530

    switchCaseQ1

---
+++ switchCaseQ2.java
@@ -0,0 +1,14 @@
+// Dispay name of a Month based on number
+
+import java.util.Scanner;
+
+public class switchCaseQ2 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number !");
+        int num;
+        num=sc.nextInt();
+
+
+    }
+}
